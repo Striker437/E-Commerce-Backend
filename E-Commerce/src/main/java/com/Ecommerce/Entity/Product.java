1c68a5e940753d41810151162d34405abaece252
anuragsoni2009 <anuragsoni2009@gmail.com>
2020-12-18 19:16:26 +0530

    Code Formatting and adding default constructor to the entity class

--- E-Commerce/src/main/java/com/Ecommerce/Entity/Product.java
+++ E-Commerce/src/main/java/com/Ecommerce/Entity/Product.java
@@ -8,10 +8,9 @@ import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
-
 @Entity
 public class Product {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	int id;
@@ -22,65 +21,84 @@ public class Product {
 	String imageURL;
 	@ManyToOne
 	Category category;
-	
+
 	@JsonBackReference
-    @ManyToOne 
-    Cart cart;
-	
+	@ManyToOne
+	Cart cart;
+
 	/*
 	 * @ManyToOne Order order;
 	 */
-	
-	 
+
+	Product() {
+
+		/*
+		 * default constructor
+		 */
+	}
+
 	public Cart getCart() {
 		return cart;
 	}
+
 	public void setCart(Cart cart) {
 		this.cart = cart;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getPrice() {
 		return price;
 	}
+
 	public void setPrice(int price) {
 		this.price = price;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public int getQuantity() {
 		return quantity;
 	}
+
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
+
 	public String getImageURL() {
 		return imageURL;
 	}
+
 	public void setImageURL(String imageURL) {
 		this.imageURL = imageURL;
 	}
+
 	public Category getCategory() {
 		return category;
 	}
+
 	public void setCategory(Category category) {
 		this.category = category;
 	}
-	 
-	
 
 }
